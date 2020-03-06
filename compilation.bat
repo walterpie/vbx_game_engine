c428ce1a5534b276f8cbb0bbe627d76bca4b5920
VertexByte <vertex.byte148@gmail.com>
2020-03-06 21:32:28 +0100

    added the winmm.lib for beginTimePeriod

--- compilation.bat
+++ compilation.bat
@@ -2,5 +2,5 @@
 
 
 cd build
-cl -Zi -nologo ../vertexbyte_win32.cpp /Fe"vertexbyte_win32.exe" /link user32.lib gdi32.lib
+cl -Zi -nologo ../vertexbyte_win32.cpp /Fe"vertexbyte_win32.exe" /link user32.lib gdi32.lib winmm.lib
 cd ..
