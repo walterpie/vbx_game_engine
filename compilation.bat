5bc1e6c02ffe78ba3d45d1a6569012c2a73db581
VertexByte <vertex.byte148@gmail.com>
2020-03-08 21:21:30 +0100

    make the compilation file cleaner.

--- compilation.bat
+++ compilation.bat
@@ -1,6 +1,11 @@
 @echo off
 
+set switches=-Zi -nologo 
+set linker_flags=/link user32.lib gdi32.lib winmm.lib /incremental:no
+set dll_linker_flags=/link /DLL /EXPORT:game_update_and_render /EXPORT:initialize_window_state /PDB:vertexbyte_%random%.pdb  /incremental:no
 
 cd build
-cl -Zi -nologo ../vertexbyte_win32.cpp /Fe"vertexbyte_win32.exe" /link user32.lib gdi32.lib winmm.lib
+del *.pdb
+cl %switches% ../vertexbyte.cpp /Fe"vertexbyte.dll"  %dll_linker_flags% 
+cl %switches% ../vertexbyte_win32.cpp /Fe"vertexbyte_win32.exe" %linker_flags% 
 cd ..
